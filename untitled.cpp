ea3d795b4c5bbebb87a9773e90e18fe3b3b88676
ZARD <Jason@huangjiandeMacBook-Pro.local>
2015-10-22 00:38:02 +0800

    haode

---
+++ untitled.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int k[30];
+void ok(int *a,int b,int c)
+{
+
+}
+int main()
+{
+	int n;
+	while(cin>>n)
+	{
+		int m[30];
+		memset(m,0,sizeof(m));
+		memset
+		ok(m,1,n);
+	}
+}
